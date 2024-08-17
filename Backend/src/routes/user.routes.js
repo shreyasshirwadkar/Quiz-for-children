31923a2f446ebf2ccee8153acded760d8f273b0b
Art3mis20726 <gitpats@gmail.com>
2024-08-17 10:51:53 +0530

    "Update HTTP method for /logoutUser route from GET to PATCH"

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { logInUser, logOutUser, registerUser } from "../controllers/user.controllers.js";
 const router=Router()
 router.route("/registerUser").post(registerUser)
-router.route("/logoutUser").get(logOutUser)
+router.route("/logoutUser").patch(logOutUser)
 router.route("/loginUser").post(logInUser)
 export default router
